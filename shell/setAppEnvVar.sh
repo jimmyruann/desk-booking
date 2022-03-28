5f6730e0f279c27e435b0c8f93aeba06b052ee2f
jimmyruann <jimmyruan@hotmail.com>
2022-03-28 13:29:57 +1100

    fix nginx config

--- shell/setAppEnvVar.sh
+++ shell/setAppEnvVar.sh
@@ -2,9 +2,6 @@ if [ -z "$1" ]; then
     echo "No argument supplied"
 
 else
-    sudo apt update
-    sudo apt install jq
-
     for s in $(echo $1 | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]"); do
         export $s
     done
